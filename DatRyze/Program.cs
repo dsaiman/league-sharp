dffe6aa964cb20e7b4412ff8ba14e7cea5c6ceec
GoldenGates <mandeep_3numbers@hotmail.com>
2014-12-29 17:54:37 -0500

    Initial Commit

---
+++ DatRyze/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * User: GoldenGates
+ * Date: 29/12/2014
+ * Time: 5:33 PM
+ */
+using System;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace DatRyze
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Game.PrintChat("Hello World!");
+		}
+	}
+}
