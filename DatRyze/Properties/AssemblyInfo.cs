91c818ca64f42bb0a6eaa8c967cb043d2efa6844
GoldenGatesPB <mandeep_3numbers@hotmail.com>
2015-01-23 22:08:34 -0500

    Fixed 4.0 > 4.5 issue
    Framework updated to 4.5

--- DatRyze/Properties/AssemblyInfo.cs
+++ DatRyze/Properties/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DatRyze")]
-[assembly: AssemblyCopyright("Copyright 2014")]
+[assembly: AssemblyCopyright("Copyright 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
